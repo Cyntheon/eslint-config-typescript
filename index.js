5b335be7f290e952549c9d1911e6ef1074787d1b
Cynthia Chung <cynthiachung03@gmail.com>
2021-10-13 19:16:18 -0400

    v2.0.0

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
 module.exports = {
-  "extends": [
-    "eslint:recommended",
-    "google"
-  ],
+  "root": true,
+  "parser": "@typescript-eslint/parser",
   "plugins": [
+    "@typescript-eslint",
     "prefer-arrow"
   ],
+  "extends": [
+    "eslint:recommended",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended"
+  ],
   "rules": {
-    // https://github.com/TristonJ/eslint-plugin-prefer-arrow
+    "spaced-comment": "off",
+    "capitalized-comments": "off",
+    "require-jsdoc": "off",
+    "valid-jsdoc": "off",
+    "@typescript-eslint/await-thenable": "warn",
+    "@typescript-eslint/consistent-indexed-object-style": [
+      "error",
+      "index-signature"
+    ],
+    "@typescript-eslint/consistent-type-assertions": [
+      "error",
+      {
+        "assertionStyle": "as",
+        "objectLiteralTypeAssertions": "allow"
+      }
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-confusing-void-expression": "error",
     "prefer-arrow/prefer-arrow-functions": [
       "error",
       {
         "disallowPrototype": true
       }
     ],
-    "quotes": [
-      "error",
-      "double",
-      {
-        "allowTemplateLiterals": true
-      }
-    ],
-    "capitalized-comments": [
-      "error",
-      "always",
-      {
-        "ignoreConsecutiveComments": true
-      }
-    ],
-    "indent": [
-      "error",
-      2
-    ],
-    "require-jsdoc": "off",
-    "valid-jsdoc": "off",
-    "semi-style": [
-      "error",
-      "last"
-    ],
-    "comma-dangle": [
-      "error",
-      "never"
-    ]
+    "no-undef": "off"
   }
 };
